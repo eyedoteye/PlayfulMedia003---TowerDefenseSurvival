14be30ac6fa9342c257f2185db16b5f48a3ab5fc
i.i <i.i@001.run>
2017-07-30 20:56:46 -0400

    Add Tile Selection

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -2,19 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
-
+public class PlayerController : MonoBehaviour
+{
   public float speed = 1f;
 
   new private Rigidbody rigidbody;
-	// Use this for initialization
+  private LayerMask groundTile_LayerMask;
+
+  private GameObject cached_LastHitTile;
+
+  public enum Player_ControlMode 
+  {
+    Move,
+    Build
+  } private Player_ControlMode player_controlMode =
+    Player_ControlMode.Move;
+
 	void Start()
   {
     rigidbody = GetComponentInChildren<Rigidbody>();	
+    groundTile_LayerMask = LayerMask.GetMask("Ground Tile");
 	}
 	
-	// Update is called once per frame
-	void Update() {
+	void Update()
+  {
+    if(cached_LastHitTile != null)
+      cached_LastHitTile.GetComponent<MeshRenderer>().material.color = Color.white;
+
+    RaycastHit hit;
+    if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 300f, groundTile_LayerMask))
+    {
+      GameObject hitObject = hit.transform.gameObject;
+      hitObject.GetComponent<MeshRenderer>().material.color = Color.green;
+      cached_LastHitTile = hitObject;
+    }
 	}
 
   private void FixedUpdate()
@@ -22,12 +43,20 @@ public class PlayerController : MonoBehaviour {
     Vector2 leftJoystickInput;
     leftJoystickInput.x = Input.GetAxisRaw("Horizontal");
     leftJoystickInput.y = Input.GetAxisRaw("Vertical");
-    Vector3 movement = new Vector3(
-      leftJoystickInput.x,
-      0f,
-      leftJoystickInput.y);
+    
+    switch(player_controlMode)
+    {
+      case Player_ControlMode.Move:
+      {
+        Vector3 movement = new Vector3(
+          leftJoystickInput.x,
+          0f,
+          leftJoystickInput.y);
 
-    rigidbody.velocity = Vector3.zero;
-    rigidbody.AddForce(movement * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.AddForce(movement * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+
+      } break;
+    }
   }
 }
