0c08301f0c11bd7c12fd694c183d3ef0c1e0c0d6
i.i <i.i@001.run>
2017-08-04 03:12:36 -0400

    Add Enemy Spawner

---
+++ Assets/Scripts/Buildings/EnemySpawnerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawnerController : MonoBehaviour
+{
+  public GameObject enemyObject;
+  public GameObject enemyTarget;
+
+	void Start()
+  {
+    IEnumerator spawnEnemies = SpawnEnemies();
+    StartCoroutine(spawnEnemies);
+  }
+
+  IEnumerator SpawnEnemies()
+  {
+    for(;;)
+    {
+      GameObject enemyInstance = Instantiate<GameObject>(enemyObject, transform.position + enemyObject.transform.position, transform.rotation);
+      EnemyController enemyController = enemyInstance.GetComponent<EnemyController>();
+      enemyController.navigationTarget = enemyTarget;
+
+      yield return new WaitForSeconds(5f);
+    }
+  }
+}
