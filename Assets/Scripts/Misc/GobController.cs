83aad836b5e43e4ac91b9d374d043a66933a7481
i.i <i.i@001.run>
2017-08-14 18:59:20 -0400

    Shift Gob To Sphere

--- Assets/Scripts/Misc/GobController.cs
+++ Assets/Scripts/Misc/GobController.cs
@@ -2,17 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GobSpriteController : MonoBehaviour
+public class GobController : MonoBehaviour
 {
-  public Sprite[] sprites;
-  public SpriteRenderer spriteRenderer;
-
-  private void Start()
-  {
-    int spriteChoice = (int)(Random.value * sprites.Length);
-    spriteRenderer.sprite = sprites[spriteChoice];
-  }
-
   private void OnTriggerEnter(Collider other)
   {
     PlayerController playerController = other.GetComponent<PlayerController>();
