26e5fa778b114074d59a6859eb3a2133eeedb5a8
i.i <i.i@001.run>
2017-08-19 22:30:55 -0400

    Fix Build

--- Assets/Scripts/Characters/EnemyController.cs
+++ Assets/Scripts/Characters/EnemyController.cs
@@ -135,7 +135,7 @@ EnemyController : MonoBehaviour
 
     if(!Physics.SphereCast(
       new Ray(transform.position, Vector3.Normalize(navMeshAgent.desiredVelocity)),
-      0.6f,
+      0.5f,
       out hitInfo,
       Mathf.Infinity,
       ~LayerMask.NameToLayer("Attackable")))
